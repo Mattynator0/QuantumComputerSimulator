bbd72ed9777374649bb5a8768948c961aafa1292
Mattynator0 <mm6205@wp.pl>
2026-03-11 14:49:59 +0100

    moved classes related to circuit simulation to the simulator package

--- src/main/java/org/example/simulator/QuantumCircuit.java
+++ src/main/java/org/example/simulator/QuantumCircuit.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.simulator;
 
 import ch.obermuhlner.math.big.BigDecimalMath;
 import lombok.Getter;
--- src/main/java/org/example/simulator/QuantumTransformation.java
+++ src/main/java/org/example/simulator/QuantumTransformation.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.simulator;
 
 import lombok.Getter;
 
