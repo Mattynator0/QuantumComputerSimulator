df1e4281f4784cddc45532eed69c66a077d68061
Mattynator0 <mm6205@wp.pl>
2026-03-11 14:47:00 +0100

    fixed constants' names

--- src/main/java/org/example/math/BigDecimalMathHelper.java
+++ src/main/java/org/example/math/BigDecimalMathHelper.java
@@ -7,13 +7,13 @@ public class BigDecimalMathHelper {
 
     public static final MathContext MC = new MathContext(10);
 
-    public static final MathContext printMC = new MathContext(3);
+    public static final MathContext PRINT_MC = new MathContext(3);
 
-    public static final BigDecimal EPS = BigDecimal.ONE
+    public static final BigDecimal EPSILON = BigDecimal.ONE
             .movePointLeft(MC.getPrecision());
 
     public static BigDecimal clampToZero(BigDecimal x) {
-        return x.abs().compareTo(EPS) < 0 ? BigDecimal.ZERO : x;
+        return x.abs().compareTo(EPSILON) < 0 ? BigDecimal.ZERO : x;
     }
 
     public static BigDecimal zeroIfTiny(BigDecimal value) {
